f6f6fe6fdafd1c7790d116f4ab915164f092aaac
Giorgio Di Nardo <akelitz@hotmail.com>
2017-01-05 13:21:04 +0100

    Binding di base

--- src/AureliaOnCore.Web/wwwroot/app.js
+++ src/AureliaOnCore.Web/wwwroot/app.js
@@ -1,3 +1,19 @@
-﻿export class App {
+﻿export class Games {
+    constructor() {
+        this.games = [
+            { id: 1, title: 'Il labirinto perduto', master: 'Lucia', canJoin: false, canPlay: true },
+            { id: 2, title: 'La tana del drago', master: 'Roberto', canJoin: true, canPlay: false }
+        ];
+    }
 
+    create() {
+        this.games.push(
+            { id: this.games.length + 1, title : 'Nuova partita', master: 'Giorgio', canJoin: false, canPlay: true } 
+        );
+    }
+
+    join(game) {
+        game.canJoin = false;
+        game.canPlay = true;
+    }
 }
